f83ebe4e76258dfafa2a998565a6aa08df456869
Paulio <pauliorandall@gmail.com>
2025-10-15 16:46:32 +0100

    Add code implementation + tests

--- api/study/code.go
+++ api/study/code.go
@@ -1,1 +1,35 @@
 package study
+
+var codeSchema = JsonObject{
+	"type": "object",
+	"fields": SchemaPropFields{
+		"id": JsonObject{
+			"type": "string",
+		},
+		"name": JsonObject{
+			"type": "string",
+		},
+		"tags": JsonObject{
+			"type": "array",
+			"items": JsonObject{
+				"type": "string",
+			},
+		},
+		"description": JsonObject{
+			"type": "string",
+		},
+	},
+}
+
+func ValidateCode(code JsonObject) []string {
+	pErrors, err := newErrorSlice()
+
+	validateSchema(
+		err,
+		codeSchema,
+		code,
+		"code",
+	)
+
+	return *pErrors
+}
--- api/study/code_test.go
+++ api/study/code_test.go
@@ -1,1 +1,40 @@
 package study
+
+import (
+	"testing"
+)
+
+func Test_Code_1(t *testing.T) {
+	// GIVEN Valid code
+	// WHEN ValidateCode()
+	// THEN returns no errors
+
+	code := parseJson(`{
+		"id": "123",
+		"name": "Slow cursor speed",
+		"tags": [],
+		"description": "Annoyed at cursor/pointer speed"
+	}`)
+
+	errors := ValidateCode(code)
+
+	requireErrors(t, errors)
+}
+
+func Test_Code_2(t *testing.T) {
+	// GIVEN Code with missing description
+	// WHEN ValidateCode()
+	// THEN return 1 type error
+
+	code := parseJson(`{
+		"id": "123",
+		"name": "Abc",
+		"tags": ["P1"]
+	}`)
+
+	errors := ValidateCode(code)
+
+	requireErrors(t, errors,
+		"code.description: Expected 'string' but got 'unknown'",
+	)
+}
