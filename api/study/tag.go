4a13394f64be0697b229589ec3af104dd5d44458
Paulio <pauliorandall@gmail.com>
2025-10-16 12:53:36 +0100

    Add validate tag impl

--- api/study/tag.go
+++ api/study/tag.go
@@ -1,1 +1,29 @@
 package study
+
+var tagSchema = JsonObject{
+	"type": "object",
+	"fields": SchemaPropFields{
+		"id": JsonObject{
+			"type": "string",
+		},
+		"name": JsonObject{
+			"type": "string",
+		},
+		"description": JsonObject{
+			"type": "string",
+		},
+	},
+}
+
+func ValidateTag(tag JsonObject) []string {
+	pErrors, err := newErrorSlice()
+
+	validateSchema(
+		err,
+		tagSchema,
+		tag,
+		"tag",
+	)
+
+	return *pErrors
+}
--- api/study/tag_test.go
+++ api/study/tag_test.go
@@ -1,1 +1,38 @@
 package study
+
+import (
+	"testing"
+)
+
+func Test_Tag_1(t *testing.T) {
+	// GIVEN Valid tag
+	// WHEN ValidateTag()
+	// THEN returns no errors
+
+	tag := parseJson(`{
+		"id": "123",
+		"name": "P1",
+		"description": "Participant 1"
+	}`)
+
+	errors := ValidateTag(tag)
+
+	requireErrors(t, errors)
+}
+
+func Test_Tag_2(t *testing.T) {
+	// GIVEN Tag with missing description
+	// WHEN ValidateTag()
+	// THEN return 1 type error
+
+	tag := parseJson(`{
+		"id": "123",
+		"name": "P1"
+	}`)
+
+	errors := ValidateTag(tag)
+
+	requireErrors(t, errors,
+		"tag.description: Expected 'string' but got 'unknown'",
+	)
+}
