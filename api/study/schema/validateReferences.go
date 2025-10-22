648cd14c68c9ce112733cd5fcbb8cb9de8d775f8
Paulio <pauliorandall@gmail.com>
2025-10-23 00:20:08 +0100

    Start impl ref validation in API

--- api/study/schema/validateReferences.go
+++ api/study/schema/validateReferences.go
@@ -1,52 +1,115 @@
 package schema
 
 import (
-// "fmt"
+	//"fmt"
+	"strings"
 )
 
 func ValidateReferences(
 	err Err,
-	schema JsonObject,
 	data JsonObject,
+	schema JsonObject,
 	prop JsonValue,
 	propName string,
 ) {
 	if hasPropValue[string](schema, "ref") {
-		findAndCheckRef(err, schema, data, prop, propName)
-		return
+
 	}
+
+	/*
+		for fieldName, subSchema := range data["fields"] {
+			fieldPropName := propName + "." + fieldName
+			fieldValue := data[fieldName]
+
+			if hasPropValue[string](subSchema, "ref") {
+				findAndCheckRef(err, subSchema, data, fieldValue, fieldPropName)
+				continue
+			}
+
+			if getPropValue[string](subSchema, "type") == TypeObject {
+				ValidateReferences(err, subSchema, fieldValue, fieldPropName)
+			}
+		}
+	*/
 }
 
+/*
 func findAndCheckRef(
+
 	err Err,
 	schema JsonObject,
 	data JsonObject,
 	prop JsonValue,
 	propName string,
-) {
-	if getPropValue[string](schema, "type") != TypeString {
-		err("%s: Reference fields must always be a string", propName)
-		return
-	}
 
-	ref := getPropValue[string](schema, "ref")
+	) {
+		if getPropValue[string](schema, "type") != TypeString {
+			err("%s: Reference fields must always be a string", propName)
+			return
+		}
 
-	if !refExists(data, prop.(string), ref) {
-		err("%s: Reference fields must always be a string", propName)
+		if determineType(prop) != TypeString {
+			err("%s: Reference fields must always be a string", propName)
+			return
+		}
+
+		ref := getPropValue[string](schema, "ref")
+
+		if !refExists(data, prop.(string), ref) {
+			err("%s: Reference doesn't exist at '%s'", propName, ref)
+		}
 	}
-}
 
 func refExists(
+
 	data JsonObject,
 	value string,
 	ref string,
-) bool {
-	// TODO:
-	//   1: split ref by '.'
-	//   2: descend 'data' to find ref
-	//   3: if can't descend to end of ref then return false
-	//   4: if id/value found that matches 'ref' and 'value' then return true
-	//   5: else return false
 
+	) bool {
+		path := strings.Split(ref, ".")
+		pArray := getRefArray(data, path...)
+
+		fmt.Printf("pArray: %s", *pArray)
+
+		if pArray == nil {
+			return false
+		}
+
+		array, ok := (*pArray).(JsonArray)
+
+		if !ok {
+			return false
+		}
+
+		return arrayContains(array, value)
+	}
+*/
+func findArray(data JsonObject, ref string) JsonValue {
+	var result JsonValue = JsonValue(data)
+	path := strings.Split(ref, ".")
+
+	for _, segment := range path {
+		obj, isObject := result.(JsonObject)
+		if !isObject {
+			return nil
+		}
+
+		if v, ok := obj[segment]; ok {
+			result = v
+		} else {
+			return nil
+		}
+	}
+
+	return result
+}
+
+func arrayContains(haystack JsonArray, needle string) bool {
+	for _, v := range haystack {
+		if v == needle {
+			return true
+		}
+	}
 	return false
 }
--- api/study/schema/validateReferences_test.go
+++ api/study/schema/validateReferences_test.go
@@ -2,6 +2,8 @@ package schema
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func testValidateReferences(schemaName string, schema JsonObject, data JsonValue) []string {
@@ -9,8 +11,8 @@ func testValidateReferences(schemaName string, schema JsonObject, data JsonValue
 
 	ValidateReferences(
 		err,
-		schema,
 		data.(JsonObject),
+		schema,
 		data,
 		schemaName,
 	)
@@ -18,6 +20,56 @@ func testValidateReferences(schemaName string, schema JsonObject, data JsonValue
 	return *pErrors
 }
 
+func Test_findArray_1(t *testing.T) {
+	// GIVEN Data with array at top level
+	// WHEN searching for that array
+	// THEN returns it
+
+	data := parseJson(`{
+		"ids": ["abc", "123"]
+	}`)
+
+	act := findArray(data, "ids")
+	exp := []JsonValue{"abc", "123"}
+	require.EqualValues(t, exp, act)
+}
+
+func Test_findArray_2(t *testing.T) {
+	// GIVEN Data with array not at top level
+	// WHEN searching for that array
+	// THEN returns it
+
+	data := parseJson(`{
+		"things": {
+			"stuff": {
+				"ids": ["abc", "123"]
+			}
+		}
+	}`)
+
+	act := findArray(data, "things.stuff.ids")
+	exp := []JsonValue{"abc", "123"}
+	require.EqualValues(t, exp, act)
+}
+
+func Test_findArray_3(t *testing.T) {
+	// GIVEN Search ref not matching data structure
+	// WHEN searching for referenced array
+	// THEN returns nil
+
+	data := parseJson(`{
+		"things": {
+			"thongs": {
+				"ids": ["abc", "123"]
+			}
+		}
+	}`)
+
+	act := findArray(data, "things.stuff.ids")
+	require.EqualValues(t, nil, act)
+}
+
+/*
 func Test_ValidateReferences_1(t *testing.T) {
 	// GIVEN Valid
 	// WHEN passed valid data
@@ -28,9 +80,9 @@ func Test_ValidateReferences_1(t *testing.T) {
 		"fields": SchemaPropFields{
 			"thingId": JsonObject{
 				"type": "string",
-				"ref":  "things",
+				"ref":  "thingIds",
 			},
-			"things": JsonObject{
+			"thingIds": JsonObject{
 				"type": "array",
 				"items": JsonObject{
 					"type": "string",
@@ -40,10 +92,12 @@ func Test_ValidateReferences_1(t *testing.T) {
 	}
 
 	data := parseJson(`{
-		"thingId": "123",
-		"things": ["123"]
+		"thingId": "12",
+		"thingIds": ["123"]
 	}`)
 
 	errors := testValidateReferences("test", schema, data)
+	t.Fail()
 	requireErrors(t, errors)
 }
+*/
